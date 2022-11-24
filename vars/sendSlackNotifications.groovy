9ffd3d7ea86a389ecea8ed358beccee77458f3cd
rakhidevops1 <115344880+rakhidevops1@users.noreply.github.com>
2022-11-24 23:08:08 +0530

    Update sendSlackNotifications.groovy

--- vars/sendSlackNotifications.groovy
+++ vars/sendSlackNotifications.groovy
@@ -36,5 +36,5 @@ def call(String buildStatus = 'STARTED') {
       }
     
       // Send notifications
-      slackSend (color: colorCode, message: summary, channel: "#walmart-dev")
+      slackSend (color: colorCode, message: summary, channel: "#walmartdev")
     }
